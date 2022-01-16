363e0219a1c47dc6c0a38e33186b31f3f83f0a68
mohammedismayil <mohammedismayil10@gmail.com>
2022-01-16 18:53:33 +0530

    somewhat completed api interactor

--- Swift/Machine.swift
+++ Swift/Machine.swift
@@ -15,3 +15,22 @@ extension Vehicle{
         print("honk honk")
     }
 }
+class Car: Vehicle{
+     func drive() {
+    print("let's go to car drive")
+    }
+
+}
+extension Vehicle{
+    func horn(){
+        print("honk honk")
+    }
+
+    func lightOn(){
+        print("darkness removed")
+    }
+
+    func closeDoor(){
+        print("closing door automatically")
+    }
+}
--- Swift/Main.swift
+++ Swift/Main.swift
@@ -2,24 +2,9 @@ let newVehicle =  Bike();
 
 newVehicle.didLoad()
 
-// let newCar = Car();
 
-// newCar.drive();
-// newCar.closeDoor();
-
-protocol Vehicle{
-    
-    func drive()
-}
-
-class Car: Vehicle{
-     func drive() {
-    print("let's go to car drive")
-    }
-
-}
 class Bike: APIProtocol{
-var interactor: APIInteractor
+var interactor: APIInteractor?
 func onSuccess() {
 print("success from bike")
 }
@@ -28,63 +13,50 @@ print("damn these coding- from bike class")
 }
 
 func didLoad(){
-   self.interactor = APIInteractorClass(view:self)
-    self.interactor.getData()
+   
+    self.interactor?.getData()   
 }
 
 
 init() {
-    self.interactor = APIInteractorClass(view:self)
+    //  self.interactor = APIInteractorClass(view:self)
+    self.interactor = APIInteractorClass.init(view: self)
 }
+
+
 
    
 }
-// protocol Car {
-// var name:    
-// }
+
 protocol APIProtocol{
-    var interactor: APIInteractor{get set}
+    var interactor: APIInteractor?{get set}
     func onSuccess()
     func onFailure()
 }
 
 protocol APIInteractor{
-
-    var view:APIProtocol{get set}
-
+    var view:APIProtocol?{get set}
     func sample()
     
 }
 extension APIInteractor{
     func getData(){
-self.view.onSuccess()
+self.view?.onFailure()
     }
 
-    
-    init(view:APIProtocol) {
-        self.view = view
-    }
-    
+
+//  init(view:APIProtocol){
+//     self.view = view
+// }
 }
 class APIInteractorClass: APIInteractor{
-      var view:APIProtocol
+      var view:APIProtocol?
 
     func sample(){
         print("items: Any...")
     }
-
-    
+init(view:APIProtocol) {
+    self.view = view
 }
-extension Vehicle{
-    func horn(){
-        print("honk honk")
-    }
-
-    func lightOn(){
-        print("darkness removed")
-    }
-
-    func closeDoor(){
-        print("closing door automatically")
-    }
+    
 }
